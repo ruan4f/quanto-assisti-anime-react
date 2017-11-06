f8aa60af8a5aa2b2b149ae77b1d508d766abde07
Ruan <ruan4f@gmail.com>
2017-11-06 14:25:50 -0200

    Example todo item list

--- src/views/list-my-animes.js
+++ src/views/list-my-animes.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react'
-import TodoApp from '../components/TodoItem'
+import TodoList from '../components/TodoList'
 
 class ListMyAnimes extends Component {
-
-
-
-
     render() {
-        var todoItems = [];
-        todoItems.push({ index: 1, value: "learn react", done: false });
-        todoItems.push({ index: 2, value: "Go shopping", done: true });
-        todoItems.push({ index: 3, value: "buy flowers", done: true });
+        
         return (
             <div>
                 <h1>List my animes - componente que vai listar os animes que estou assistindo</h1>
-                <TodoApp initItems={todoItems} />
-
+                <TodoList />
             </div>
         )
     }
 }
 
 export default ListMyAnimes
-
